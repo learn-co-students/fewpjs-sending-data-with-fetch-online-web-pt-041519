84b4821c724a8e1b5af18d8e49a6ddd5f379919b
blackwellsmith <blackwellsmithiv@gmail.com>
2019-11-08 18:02:51 -0500

    Done.

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 
 function submitData(name, email) {
     
-    fetch('http://localhost:3000/users', {
+    return fetch('http://localhost:3000/users', {
         method: 'Post',
         headers: { 'Content-Type': 'application/json', 'accept': 'application/json' },
         body: JSON.stringify({ name, email })
     }).then(res => { return res.json() }).then(json => document.body.innerHTML = json["id"]).catch(function(error) {
         alert("Unauthorized Access")
-        documt.body.innerHTML = error.message
+        document.body.innerHTML = error.message
       })
 } 
