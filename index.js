fedce886c4cd7bce3bc2dbd350e8619bb4004db7
joannayhs <joannayuhas@gmail.com>
2019-11-12 20:59:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function  submitData(name, email) {
+    let configObject = {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json", 
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    };
+
+    return fetch('http://localhost:3000/users', configObject)
+    .then( function(res) {
+        return res.json()
+    })
+    .then( function(object) {
+        document.body.innerHTML = object["id"]
+    })
+    .catch(function(error) {
+        document.body.innerHTML = error.message
+    })
+}
+
