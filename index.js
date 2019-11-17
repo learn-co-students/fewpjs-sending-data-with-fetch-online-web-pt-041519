f639b58619578b0b214c14eb644e4c09a28fe4b1
earthctzn <earthctzn1@gmail.com>
2019-11-17 16:20:23 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your code here
+
+
+const submitData = (name, email) => {
+  let formData = {
+    name: name,
+    email: email
+  }
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+  body: JSON.stringify(formData)
+  })
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    document.body.innerHTML = object["id"]
+  })
+  .catch(function(error) {
+    document.body.innerHTML = error.message
+  })
+}
+
+
+
+
+
+
+
+
+
+
