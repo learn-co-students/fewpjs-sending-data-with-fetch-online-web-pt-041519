1742530402624c1f3feaf074e99ac10c821743d2
Garth Lilly <garff247@gmail.com>
2019-12-17 14:43:55 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+
+
+function submitData(name, email) {
+    let configObj = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: `${name}`,
+            email: `${email}`
+        })
+    };
+
+    return fetch('http://localhost:3000/users', configObj)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object) {
+            const body = document.querySelector('body')
+            body.innerHTML += `<h1>${object.id}</h1>`
+        })
+        .catch(function(error) {
+            const body = document.querySelector('body')
+            body.innerHTML += error
+        })
+    
+};
+
+//submitData();
